70e4c43bda1530793d102c45224e30f0f7450960
GuilhermeAOliveira <tikanus@gmail.com>
2022-08-02 15:52:45 -0300

    Esboço dos testes E2E

---
+++ cypress/e2e/bees.js
@@ -0,0 +1,13 @@
+import {SigninPage} from "../page-objects/signinPage"
+
+const signinPage = new SigninPage();
+
+context('To be filled', () => {
+    beforeEach(() => {
+        clearAll();
+    })
+
+    it("To be filled", ()=> {
+      //  
+    })
+})
